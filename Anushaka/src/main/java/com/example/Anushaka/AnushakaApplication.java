73f1ba77834047e08d4e1c8c2b1c52d130683e52
Pavan <pavanwavhal1@gmail.com>
2022-11-12 18:29:11 +0530

    Change

---
+++ Anushaka/src/main/java/com/example/Anushaka/AnushakaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Anushaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnushakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnushakaApplication.class, args);
+	}
+
+}
---
+++ Anushaka/src/test/java/com/example/Anushaka/AnushakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Anushaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnushakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
